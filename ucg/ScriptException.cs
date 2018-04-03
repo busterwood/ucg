7d2225ffd415be8000db007ef0dc03f14ba9f65c
BusterWood <chris.j.austin@gmail.com>
2018-04-03 16:24:16 +0100

    Initial commit

---
+++ ucg/ScriptException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ucg
+{
+    [Serializable]
+    internal class ScriptException : Exception
+    {
+        public ScriptException()
+        {
+        }
+
+        public ScriptException(string message) : base(message)
+        {
+        }
+
+        public ScriptException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ScriptException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
